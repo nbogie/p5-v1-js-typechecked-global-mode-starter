a288bbcf28576bee964b6b1a7517911ed859d193
nbogie <neillbogie@googlemail.com>
2025-11-26 06:15:52 +0000

    rename biggerSketch to starsSketch

--- more-examples/demoOfProblemWithoutES2015.js
+++ more-examples/demoOfProblemWithoutES2015.js
@@ -1,5 +1,6 @@
 //this requires you set a target of ES2015 or later
 const myArray = [10, 20, 30];
+//try removing target from jsconfig.json and see what happens with the following line
 for (const entry of myArray.entries()) {
     console.log(entry);
 }
